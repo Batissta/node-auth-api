61a99d32511307025cd8c091da6645fe4eb9bd1d
Batissta <francinaldofilho05@gmail.com>
2025-03-11 20:11:49 -0300

    Refactor - Tratando possíveis erros na conexão com a database

--- src/app.js
+++ src/app.js
@@ -9,10 +9,15 @@ app.use(express.json());
 app.use(middleware);
 app.use(pessoaRoute);
 
-mongoose.connect(process.env.MONGO_STRING_CONNECTION).then(() => {
-  console.log("O banco de dados está rodando!");
-  app.emit("ok");
-});
+mongoose
+  .connect(process.env.MONGO_STRING_CONNECTION)
+  .then(() => {
+    console.log("O banco de dados está rodando!");
+    app.emit("ok");
+  })
+  .catch(() => {
+    console.log("A api não conseguiu se conectar com o banco de dados!");
+  });
 
 app.on("ok", () => {
   app.listen(process.env.PORT, () => {
