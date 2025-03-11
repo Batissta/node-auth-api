fc864f22860092227a0f5fde291d0c264db52bdd
Batissta <francinaldofilho05@gmail.com>
2025-03-11 20:10:52 -0300

    Feat - Utilizando o middleware no servidor

--- src/app.js
+++ src/app.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const middleware = require("./middleware");
 const pessoaRoute = require("./routes/pessoa");
 const app = express();
 
 app.use(express.json());
+app.use(middleware);
 app.use(pessoaRoute);
 
-mongoose
-  .connect(process.env.MONGO_STRING_CONNECTION)
-  .then(() => {
-    console.log("A api se conectou com o banco de dados!");
-    app.emit("ok");
-  })
-  .catch(() => {
-    console.log("A api não conseguiu se conectar com o banco de dados!");
-  });
+mongoose.connect(process.env.MONGO_STRING_CONNECTION).then(() => {
+  console.log("O banco de dados está rodando!");
+  app.emit("ok");
+});
 
 app.on("ok", () => {
   app.listen(process.env.PORT, () => {
-    console.log("O servidor está rodando!");
+    console.log(`O servidor está rodando! Na porta ${process.env.PORT}.`);
   });
 });
