76bb27f5c753c2614e84e09a25df36d3635aa60c
Batissta <francinaldofilho05@gmail.com>
2025-03-11 20:17:07 -0300

    Feat - Adicionando atributo único ao model de pessoa

--- src/models/pessoa.js
+++ src/models/pessoa.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
 const pessoaSchema = new mongoose.Schema({
-  nome: { type: String, required: true },
+  apelido: { type: String, required: true, unique: true },
   idade: { type: Number, required: true },
-  telefone: { type: String, required: true },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  senha: { type: String, required: true },
 });
 
 module.exports = mongoose.model("pessoas", pessoaSchema);
